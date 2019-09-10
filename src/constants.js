e70a6801089358b638e03539bde1089a32c35c5a
Mark1Z <muslim1992@gmail.com>
2019-09-10 16:06:23 +0300

    rename command

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const INDENT_TEXT_ATTRIBUTE = 'indent';
-export const INDENT_TEXT_COMMAND = 'indent';
+export const INDENT_TEXT_ATTRIBUTE = 'indent-text';
+export const INDENT_TEXT_COMMAND = 'indent-text';
 export const INDENT_TEXT_DEFAULT_MEASURE = 'px';
 export const INDENT_TEXT_DEFAULT_VALUE = 40;
